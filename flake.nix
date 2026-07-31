0e3100dd3fc1a4da7f215266463ba2bfdd99d516
felipeasop <flpasop27@example.com>
2026-07-31 19:46:57 -0300

    chore(flake): atualizar inputs e lockfile

--- flake.nix
+++ flake.nix
@@ -21,6 +21,13 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     noctalia.url = "github:noctalia-dev/noctalia";
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs = {
+        home-manager.follows = "home-manager";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
