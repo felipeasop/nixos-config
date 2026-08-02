1c8e093418e4e2a6779aa162c27d0bd2cfd72bd5
Felipe Augusto <flpasop27@gmail.com>
2026-08-02 16:08:19 -0300

    add(zed): adicionando configurações

--- modules/apps/dev/zed-editor.nix
+++ modules/apps/dev/zed-editor.nix
@@ -3,9 +3,21 @@
     homeManager = {
       programs.zed-editor = {
         enable = true;
+
+        extensions = [
+          "nix"
+          "toml"
+        ];
+
         userSettings = {
           buffer_font_family = "JetBrainsMono Nerd Font";
           ui_font_family = "JetBrainsMono Nerd Font";
+          buffer_font_size = 14;
+          ui_font_size = 14;
+
+          theme = {
+            mode = "system"
+          };
         };
       };
     };
