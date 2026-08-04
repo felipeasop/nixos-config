9b06f3689cbaa2d47aa4ac4f871df6fde93d5c6f
felipeasop <flpasop27@example.com>
2026-08-03 21:09:39 -0300

    chore(pre-commit): statix ignora _hardware.nix autogerado

--- modules/system/pre-commit.nix
+++ modules/system/pre-commit.nix
@@ -8,7 +8,13 @@
         entry = lib.mkForce (lib.getExe self'.formatter);
       };
       deadnix.enable = true;
-      statix.enable = true;
+      statix = {
+        enable = true;
+        # _hardware.nix é autogerado pelo nixos-generate-config
+        # (cabeçalho "DO NOT MODIFY")
+        # Excluído por padrão de nome, cobre qualquer host
+        excludes = [ "_hardware\\.nix$" ];
+      };
     };
     pre-commit.check.enable = false;
   };
