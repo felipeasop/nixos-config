566666111948fefa098820d2b7ee376004175ac5
felipeasop <flpasop27@example.com>
2026-08-06 20:16:52 -0300

    feat(zen-browser): adiciona extensões via policies

--- modules/apps/browsers/zen-browser.nix
+++ modules/apps/browsers/zen-browser.nix
@@ -13,6 +13,18 @@
       programs.zen-browser = {
         enable = true;
         setAsDefaultBrowser = true;
+
+        policies = let
+          mkExtensionSettings = builtins.mapAttrs (_: pluginId: {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
+            installation_mode = "force_installed";
+          });
+        in {
+          ExtensionSettings = mkExtensionSettings {
+            "uBlock0@raymondhill.net" = "ublock-origin";
+            "{85860b32-02a8-431a-b2b1-40fbd64c9c69}" = "github-file-icons";
+          };
+        };
       };
     };
   };
