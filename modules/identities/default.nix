f1a307c7d14cddca4cb15b503bd984eb6ad3f81f
felipeasop <flpasop27@example.com>
2026-08-07 21:35:14 -0300

    style(identities): formatacao de codigo

--- modules/identities/default.nix
+++ modules/identities/default.nix
@@ -1,5 +1,6 @@
 { lib, den, ... }: {
-  den.aspects.identities = { host, user }: with den.aspects; {
+  den.aspects.identities = { host, user }: with den.aspects;
+    {
       includes =
         lib.optionals (user.isGaming or false) [ gaming ]
         ++ lib.optionals (host.isLaptop or false) [ laptop ];
