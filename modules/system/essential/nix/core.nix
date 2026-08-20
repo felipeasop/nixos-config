05330d4e25c031f7b2188d4d44322830cca80840
felipeasop <flpasop27@gmail.com>
2026-08-20 18:50:35 -0300

    feat(nix): permitir pacotes unfree em comandos avulsos
    Declara a configuração pessoal do nixpkgs via Home Manager para que comandos como nix run nixpkgs#<pacote> aceitem software unfree sem flags imperativas.

--- modules/system/essential/nix/core.nix
+++ modules/system/essential/nix/core.nix
@@ -1,12 +1,18 @@
 { inputs, ... }: {
   den.aspects.essential = {
     nixos.system.stateVersion = "26.05";
-    homeManager.home.stateVersion = "26.05";
+    homeManager = {
+      home.stateVersion = "26.05";
+
+      # Usado por comandos avulsos, como `nix run nixpkgs#<pacote>`.
+      xdg.configFile."nixpkgs/config.nix".text = ''
+        { allowUnfree = true; }
+      '';
+    };
     nixos = {
       imports = [ inputs.home-manager.nixosModules.default ];
 
       nixpkgs.config.allowUnfree = true;
-
       home-manager = {
         useGlobalPkgs = true;
         useUserPackages = true;
