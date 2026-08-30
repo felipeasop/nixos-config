eddb50438c2389a13418e100717fa00261e287e6
felipeasop <flpasop27@gmail.com>
2026-08-30 17:50:45 -0300

    chore(nix): adicionar cache rstats-on-nix
    Adiciona o substituter e a chave pública do cache rstats-on-nix à configuração global do Nix.\n\nMáquinas reconstruídas a partir desta configuração podem reutilizar artefatos publicados por esse cache.

--- modules/system/essential/nix/nix-settings.nix
+++ modules/system/essential/nix/nix-settings.nix
@@ -21,12 +21,14 @@
             "https://chaotic-nyx.cachix.org"
             "https://nix-community.cachix.org"
             "https://noctalia.cachix.org"
+            "https://rstats-on-nix.cachix.org"
           ];
           trusted-public-keys = [
             "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
             "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
             "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+            "rstats-on-nix.cachix.org-1:vdiiVgocg6WeJrODIqdprZRUrhi1JzhBnXv7aWI6+F0="
           ];
 
           # Usa todos os núcleos disponíveis tanto pra rodar builds em paralelo
